697dfc2e1bc444020e7e95dc4f0fd9c0e4ebbc84
kleber <kleber.andrade@gmail.com>
2025-08-04 22:38:54 -0300

    Configuração Https9 G

--- .platform/hooks/prebuild/01_get_ssl.sh
+++ .platform/hooks/prebuild/01_get_ssl.sh
@@ -5,20 +5,23 @@ EMAIL="kleber.andrade@gmail.com"
 DOMAIN1="gestaopromo.com.br"
 DOMAIN2="www.gestaopromo.com.br"
 
-# --- Nova Abordagem: Instalação Autocontida do Certbot com pip ---
-echo "=== Preparando uma instalação autocontida do Certbot ==="
+echo "=== Instalando dependências para o ambiente virtual do Certbot ==="
+# Instala o pacote python3-venv, que é essencial para criar ambientes virtuais com o comando 'python3 -m venv'
+sudo dnf install -y python3-venv gcc python3-devel
 
+echo "=== Preparando uma instalação autocontida do Certbot ==="
 # 1. Cria um ambiente virtual para o Certbot não interferir com a aplicação
-sudo python3 -m venv /opt/certbot/
+sudo python3 -m venv /opt/certbot_venv
+
 # 2. Ativa o ambiente e instala o Certbot usando pip
-sudo /opt/certbot/venv/bin/pip install --upgrade pip
-sudo /opt/certbot/venv/bin/pip install certbot
+sudo /opt/certbot_venv/bin/pip install --upgrade pip
+sudo /opt/certbot_venv/bin/pip install certbot
 
 # 3. Cria um link simbólico para facilitar a execução do comando
-sudo ln -s /opt/certbot/venv/bin/certbot /usr/bin/certbot
+sudo ln -sf /opt/certbot_venv/bin/certbot /usr/bin/certbot
 
 echo "=== Gerando certificado Let's Encrypt ==="
-# Verifica se o certificado já existe para evitar atingir os limites de solicitação
+# Verifica se o certificado já existe para evitar atingir os limites de taxa
 if [ ! -d "/etc/letsencrypt/live/$DOMAIN1" ]; then
     echo "Certificado não encontrado. Obtendo um novo certificado..."
     # Para o Nginx temporariamente para liberar a porta 80 para a validação
