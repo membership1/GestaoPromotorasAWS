55d9b86eb5943e6aae6b5bb553d304bbb709a21b
kleber <kleber.andrade@gmail.com>
2025-08-04 23:35:36 -0300

    Configuração Https18 G

--- .platform/hooks/prebuild/01_get_ssl.sh
+++ .platform/hooks/prebuild/01_get_ssl.sh
@@ -6,25 +6,22 @@ DOMAIN1="gestaopromo.com.br"
 DOMAIN2="www.gestaopromo.com.br"
 WEBROOT_PATH="/var/www/html"
 
-# Instala o Certbot apenas se ele ainda não estiver instalado
+# Instala o Certbot e o plugin do Nginx apenas se ainda não estiverem instalados
 if ! command -v certbot &> /dev/null
 then
     echo "=== Instalando dependências e Certbot via PIP ==="
     sudo dnf install -y python3-pip gcc python3-devel
-    sudo pip3 install certbot
+    sudo pip3 install certbot certbot-nginx
 fi
 
 echo "=== Preparando o diretório webroot para validação ==="
 sudo mkdir -p $WEBROOT_PATH
-sudo chown -R ec2-user:webapp $WEBROOT_PATH # Garante que o Nginx (webapp user) possa ler
-sudo chmod -R 755 $WEBROOT_PATH
+sudo chown -R nginx:nginx $WEBROOT_PATH
 
-echo "=== Gerando/Renovando certificado Let's Encrypt (modo Webroot) ==="
-# O --deploy-hook reinicia o Nginx apenas se um novo certificado for emitido.
-sudo certbot certonly --non-interactive --agree-tos \
+echo "=== Gerando/Renovando certificado (modo Nginx) ==="
+# O plugin --nginx vai ler nossa configuração, fazer a validação e instalar o certificado
+sudo certbot --nginx --non-interactive --agree-tos \
     --email "$EMAIL" \
-    --webroot -w $WEBROOT_PATH \
-    -d "$DOMAIN1" -d "$DOMAIN2" \
-    --deploy-hook "sudo systemctl reload nginx"
+    -d "$DOMAIN1" -d "$DOMAIN2"
 
 echo "=== Script de SSL concluído com sucesso! ==="
